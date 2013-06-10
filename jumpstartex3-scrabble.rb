af2e209873e859cfec0cf741f6cbabc7b8a86ea2
theoretick <mail@limitlesschannels.com>
2013-06-09 22:01:12 -0700

    scrabble now almost works and has tests

--- jumpstartex3-scrabble.rb
+++ jumpstartex3-scrabble.rb
@@ -6,32 +6,47 @@
 #  at https://github.com/JumpstartLab/warmup-exercises/
 #
 
+require 'minitest/autorun'
 
 class Scrabble
 
   def self.score(word)
     total = 0
     word.downcase.split('').each do |character|
-      letter(character)
+      total += letter(character)
+      puts total
     end
     return total
   end
 
   def self.letter(inletter)
-    # val = {1:,2:,3:,4:,5:,8:,10:}
-    val1 = /[aeioulnrst]{1}/i
-    if inletter =~ val1
-      return 1
-    else
-      return 0
-    # # val2 = /[dg]{1}/i
-    # val3 = /[bcmp]{1}/i
-    # val4 = /[fhvwy]{1}/i
-    # val5 = /[k]{1}/i
-    # val8 = /[jx]{1}/i
-    # val10 = /[qz]{1}/i
+
+    point_store = {1 =>/[aeioulnrst]{1}/i, 2 => /[dg]{1}/i,
+      3 => /[bcmp]{1}/i, 4 => /[fhvwy]{1}/i, 5 => /[k]{1}/i,
+      8 => /[jx]{1}/i, 10 => /[qz]{1}/i}
+
+    point_store.each_pair do |key,val|
+      if inletter =~ val
+        return key
+      else
+        return 0
+      end
     end
   end
 end
 
-puts Scrabble.score('hello')
+##################################################
+# TESTS
+##################################################
+
+class TestScrabble < MiniTest::Test
+  def test_hello_is_8_points
+    assert_equal 8, Scrabble.score('hello')
+  end
+  def test_tricksysacul_is_22_points
+    assert_equal 22, Scrabble.score('tricksysacul')
+  end
+  def test_non_alphanumerics_are_0_points
+    assert_equal 4, Scrabble.score('&ba')
+  end
+end
